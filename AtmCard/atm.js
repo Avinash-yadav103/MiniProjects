d10b80e5c1e4a6dc2e4d45620748de35ebb5633c
Avinash-yadav103 <avinash.yadav02102004@gmail.com>
2024-05-29 10:43:24 +0530

    frontcompleted

---
+++ AtmCard/atm.js
@@ -0,0 +1,12 @@
+// let a = document.querySelector('.main-card');
+
+// if (a) {
+//     a.addEventListener('click', function(){
+//         a.classList.toggle('active');
+//     });
+// }
+
+function flipDiv() {
+    var flipper = document.getElementById('main-card');
+    flipper.classList.toggle('active');
+}
