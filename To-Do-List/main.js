b467abb22e123c14479e6c5d45b498d9177c5928
Avinash-yadav103 <avinash.yadav02102004@gmail.com>
2024-06-06 23:39:23 +0530

    todo

--- To-Do-List/main.js
+++ To-Do-List/main.js
@@ -1,3 +1,13 @@
-// document.body.innerHTML = '<h1>Hello from JavaScript</h1>'
+let input = document.getElementById('input');
+let ul = document.getElementById('list');
 
-// querrySelector('task1').innerHTML = InputEvent('Hello from JavaScript');
+function addTask(){
+    if(input.value === ''){
+        alert('Please enter a task');
+    }
+    else{
+        let li = document.createElement('li');
+        li.innerHTML = input.value;
+        list.appendChild(li);
+    }
+}
